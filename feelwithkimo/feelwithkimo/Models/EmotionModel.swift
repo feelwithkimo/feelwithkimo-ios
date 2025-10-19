8ba4b2c93fab1ceaf027bf5b4f9316e4280dd233
jonathancalvin <jonathancalvin16@gmail.com>
2025-10-19 20:41:12 +0700

    feat: temporary user and emotion models

---
+++ feelwithkimo/feelwithkimo/Models/EmotionModel.swift
@@ -0,0 +1,14 @@
+//
+//  EmotionModel.swift
+//  feelwithkimo
+//
+//  Created by jonathan calvin sutrisna on 19/10/25.
+//
+
+import Foundation
+
+struct EmotionModel: Identifiable, Equatable {
+    let id: UUID
+    let name: String
+    let visualCharacterName: String
+}
