3f682ae44b4a92dd06f304cde239a7ff4e56d907
Adeline Charlotte <adeline.chrltt@gmail.com>
2025-11-15 05:29:40 +0700

    refactor: cleanup dead bottom bar code

--- feelwithkimo/feelwithkimo/Features/Blocks/Views/BlocksGameView.swift
+++ feelwithkimo/feelwithkimo/Features/Blocks/Views/BlocksGameView.swift
@@ -20,127 +20,17 @@ struct BlocksGameView: View {
                 
                 Spacer()
                 
-                bottomBar
+                Rectangle()
+                    .foregroundStyle(ColorToken.backgroundHome.toColor())
+                    .frame(height: 150.getHeight())
+                    .frame(width: .infinity)
             }
             VStack{
                 Spacer()
-                bottomBar
+                bottomBar(placements: GameLevel.level1.templatePlacements)
             }
         }
         .ignoresSafeArea(edges: .all)
-    }
-    
-    private var shapesGuideCard: some View {
-        VStack(spacing: 2){
-            
-            // card title
-            HStack {
-                Text("Ayo kita buat bentuk ini!")
-                    .font(.app(.largeTitle, family: .primary))
-                    .foregroundStyle(ColorToken.backgroundSecondary.toColor())
-                    .multilineTextAlignment(.center)
-            }
-            .frame(maxWidth: .infinity)
-            .padding(.horizontal, 83.getWidth())
-            .padding(.vertical, 13.getHeight())
-            .background(ColorToken.coreAccent.toColor())
-            
-            // shapes in card
-            VStack{
-                Rectangle()
-                    .fill(Color.blue)
-                    .frame(width: 150, height: 100)
-                HStack{
-                    Rectangle()
-                        .fill(Color.blue)
-                        .frame(width: 150, height: 100)
-                    Rectangle()
-                        .fill(Color.blue)
-                        .frame(width: 150, height: 100)
-                }
-                Rectangle()
-                    .fill(Color.blue)
-                    .frame(width: 150, height: 100)
-            }
-            .padding(.horizontal, 43.getWidth())
-            .padding(.vertical, 23.getHeight())
-            .frame(maxWidth: .infinity)
-            .frame(maxHeight: .infinity)
-            .background(Color.white)
-        }
-        .frame(width: 546.getWidth())
-        .background(ColorToken.backgroundSecondary.toColor())
-        .cornerRadius(30.getHeight())
-        .overlay(
-            RoundedRectangle(cornerRadius: 30.getHeight())
-                .inset(by: -1)
-                .stroke(ColorToken.backgroundSecondary.toColor(), lineWidth: 2)
-        )
-        
-    }
-    
-    private var shapesOutlineView: some View {
-        VStack{
-            HStack {
-                Text("Ayo kita buat\nbentuk ini!")
-                    .font(.app(.largeTitle, family: .primary))
-                    .foregroundStyle(ColorToken.backgroundSecondary.toColor())
-                    .multilineTextAlignment(.center)
-            }
-            .background(ColorToken.coreAccent.toColor())
-            VStack{
-                Rectangle()
-                    .fill(Color.blue)
-                    .frame(width: 150, height: 100)
-                HStack{
-                    Rectangle()
-                        .fill(Color.blue)
-                        .frame(width: 150, height: 100)
-                    Rectangle()
-                        .fill(Color.blue)
-                        .frame(width: 150, height: 100)
-                }
-                Rectangle()
-                    .fill(Color.blue)
-                    .frame(width: 150, height: 100)
-            }
-            .padding(.horizontal, 43.getWidth())
-            .padding(.vertical, 23.getHeight())
-            .background(Color.white)
-        }
-        .padding(.horizontal, 43.getWidth())
-        .cornerRadius(30.getHeight())
-    }
-    
-    private var bottomBar: some View {
-        HStack{
-            Spacer()
-            Circle()
-                .frame(width: 150, height: 150)
-                .padding(.horizontal, 30.getWidth())
-            Circle()
-                .frame(width: 150, height: 150)
-                .padding(.horizontal, 30.getWidth())
-            Circle()
-                .frame(width: 150, height: 150)
-                .padding(.horizontal, 30.getWidth())
-            Spacer()
-        }
-        .frame(height: 150.getHeight())
-        .padding(.horizontal, 60.getWidth())
-        .background(
-                ZStack {
-                    RoundedRectangle(cornerRadius: 60, style: .continuous)
-                        .fill(ColorToken.additionalColorsLightBlue.toColor())
-                        .offset(y: -7.getHeight())
-
-                    RoundedRectangle(cornerRadius: 50, style: .continuous)
-                        .fill(ColorToken.emotionSadness.toColor())
-                }
-                .padding(.bottom, -50.getHeight())
-            )
-        .shadow(color: ColorToken.emotionSadness.toColor().opacity(0.2),
-                radius: 20, x: 0, y: -15.getHeight())
     }
 }
 
