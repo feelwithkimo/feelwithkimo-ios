ea2ef304b53c51374d41a5d7f4c87788301ee275
Adeline Charlotte <adeline.chrltt@gmail.com>
2025-11-14 14:27:11 +0700

    feat: set game level enums with level-specific block placements

--- feelwithkimo/feelwithkimo/Commons/Enums/BlockEnums.swift
+++ feelwithkimo/feelwithkimo/Commons/Enums/BlockEnums.swift
@@ -11,22 +11,80 @@ enum ShapeType: String {
     case rectangle, square, arch, triangle
 }
 
+enum RectangleRatio {
+    case ratio3to1
+    case ratio25to6
+    
+    var value: CGFloat {
+        switch self {
+        case .ratio3to1:   return 3.0 / 1.0
+        case .ratio25to6:  return 25.0 / 6.0
+        }
+    }
+}
+
+struct BlockPlacement {
+    let block: BlockModel
+    let position: CGPoint
+    let size: CGSize
+}
+
 enum GameLevel: CaseIterable {
     case level1, level2
+
     
-    var blocks: [BlockModel] {
+    var templatePlacements: [BlockPlacement] {
         switch self {
         case .level1:
             return [
-                BlockModel(type: .rectangle, color: Color.yellow),
-                BlockModel(type: .square, color: Color.green),
-                BlockModel(type: .arch, color: Color.pink)
+                BlockPlacement(
+                    block: BlockModel(type: .arch, color: ColorToken.additionalColorsLightPink.toColor()),
+                    position: CGPoint(x: 0.getWidth(), y: 260.getHeight()),
+                    size: CGSize(width: 320.getWidth(), height: 130.getHeight())
+                ),
+                BlockPlacement(
+                    block: BlockModel(type: .square, color: ColorToken.coreAccent.toColor()),
+                    position: CGPoint(x: 20.getWidth(), y: 120.getHeight()),
+                    size: CGSize(width: 120.getWidth(), height: 120.getHeight())
+                ),
+                BlockPlacement(
+                    block: BlockModel(type: .square, color: ColorToken.coreAccent.toColor()),
+                    position: CGPoint(x: 180.getWidth(), y: 120.getHeight()),
+                    size: CGSize(width: 120.getWidth(), height: 120.getHeight())
+                ),
+                BlockPlacement(
+                    block: BlockModel(type: .rectangle, color: ColorToken.additionalColorsDustyYellow.toColor()),
+                    position: CGPoint(x: 10.getWidth(), y: 0.getHeight()),
+                    size: CGSize(width: 300.getWidth(), height: 100.getHeight())
+                )
             ]
         case .level2:
             return [
-                BlockModel(type: .triangle, color: Color.yellow),
-                BlockModel(type: .rectangle, color: Color.pink),
-                BlockModel(type: .square, color: Color.blue)
+                BlockPlacement(
+                    block: BlockModel(type: .arch, color: ColorToken.additionalColorsLightPink.toColor()),
+                    position: CGPoint(x: 0.getWidth(), y: 307.getHeight()),
+                    size: CGSize(width: 100.getWidth(), height: 280.getHeight())
+                ),
+                BlockPlacement(
+                    block: BlockModel(type: .square, color: ColorToken.emotionSadness.toColor()),
+                    position: CGPoint(x: 33.getWidth(), y: 197.getHeight()),
+                    size: CGSize(width: 90.getWidth(), height: 90.getHeight())
+                ),
+                BlockPlacement(
+                    block: BlockModel(type: .square, color: ColorToken.emotionSadness.toColor()),
+                    position: CGPoint(x: 158.getWidth(), y: 197.getHeight()),
+                    size: CGSize(width: 90.getWidth(), height: 90.getHeight())
+                ),
+                BlockPlacement(
+                    block: BlockModel(type: .rectangle, color: ColorToken.additionalColorsDarkPink.toColor()),
+                    position: CGPoint(x: 15.getWidth(), y: 117.getHeight()),
+                    size: CGSize(width: 250.getWidth(), height: 60.getHeight())
+                ),
+                BlockPlacement(
+                    block: BlockModel(type: .triangle, color: ColorToken.additionalColorsDustyYellow.toColor()),
+                    position: CGPoint(x: 34.getWidth(), y: 0.getHeight()),
+                    size: CGSize(width: 212.getWidth(), height: 97.getHeight())
+                )
             ]
         }
     }
