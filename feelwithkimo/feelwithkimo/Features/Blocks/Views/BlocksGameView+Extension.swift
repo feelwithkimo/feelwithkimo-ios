b1971ea8df57c5daa03299387a716944f2c10486
Adeline Charlotte <adeline.chrltt@gmail.com>
2025-11-16 14:17:11 +0700

    chore: create View+ext file

--- feelwithkimo/feelwithkimo/Features/Blocks/Views/BlocksGameView+Extension.swift
+++ feelwithkimo/feelwithkimo/Features/Blocks/Views/BlocksGameView+Extension.swift
@@ -266,19 +266,6 @@ struct ViewPositionKey: PreferenceKey {
     }
 }
 
-extension View {
-    func readPosition(_ onChange: @escaping (CGRect) -> Void) -> some View {
-        background(
-            GeometryReader { proxy in
-                Color.clear
-                    .preference(key: ViewPositionKey.self,
-                                value: proxy.frame(in: .global))
-            }
-        )
-        .onPreferenceChange(ViewPositionKey.self, perform: onChange)
-    }
-}
-
 #Preview {
     BlocksGameView().shapesGuideCard(blockPlacements: GameLevel.level2.templatePlacements)
 }
