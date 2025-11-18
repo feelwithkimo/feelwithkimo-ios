b52ac9a5c0888b9c988a961a81c58b7fc838c62c
Adeline Charlotte <adeline.chrltt@gmail.com>
2025-11-18 17:47:02 +0700

    feat: add BlocksGamePauseView

--- feelwithkimo/feelwithkimo/Features/Blocks/Views/BlocksGamePauseView.swift
+++ feelwithkimo/feelwithkimo/Features/Blocks/Views/BlocksGamePauseView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct BlocksGamePauseView: View {
+    let onReset: () -> Void
+    let onHome: () -> Void
+    let onResume: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.black.opacity(0.7)
+            
+            VStack(alignment: .center) {
+                HStack {
+                    KimoHomeButton(isLarge: true, action: onHome)
+                    KimoResumeButton(isLarge: true, action: onResume)
+                }
+                HStack {
+                    KimoReplayButton(isLarge: true, action: onReset)
+                    KimoMuteButton(isLarge: true, audioManager: AudioManager.shared)
+                }
+            }
+        }
+        .ignoresSafeArea()
     }
 }
 
 #Preview {
-    BlocksGamePauseView()
+    BlocksGamePauseView(
+        onReset: { print("Reset") },
+        onHome: { print("Home") },
+        onResume: { print("Resume") }
+    )
 }
